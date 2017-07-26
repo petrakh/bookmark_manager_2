1bb14ba4f4d4beac30ffed419b3567eaae4cc511
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-26 12:56:08 +0200

    Filtering by tags enabled

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,9 +7,6 @@ class Tag
 
   property :id, Serial
   property :name, String
+  has n, :links, through: Resource
 
 end
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
-DataMapper.finalize
-DataMapper.auto_upgrade!
