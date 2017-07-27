fface2dab3c832a39b1fb30ef891928b39f2ef07
Petra Chocholova <petra.chocholova@gmail.com>
2017-07-27 18:46:42 +0200

    User authentication method added

--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -1,3 +1,10 @@
+def sign_in(email:, password:)
+  visit '/sessions/new'
+  fill_in :email, with: email
+  fill_in :password, with: password
+  click_button 'Sign in'
+end
+
 def sign_up
   visit '/users/new'
   expect(page.status_code).to eq(200)
